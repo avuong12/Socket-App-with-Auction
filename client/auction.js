3ab30c654e99afb96960bea30063b0b559e2cf2e
Angela Vuong <vuong.angela@gmail.com>
2020-10-21 00:31:42 -0400

    refactored auctionApp as anauction class

--- client/auction.js
+++ client/auction.js
@@ -1,26 +1,34 @@
-function makeBid(event) {
-  event.preventDefault();
-  let inputBid = document.getElementById('bid');
-  console.log(socket);
-  socket.emit('send_bid', inputBid.value);
-  inputBid.value = '';
-  return false;
-}
+class Auction {
+  constructor(socket) {
+    this.socket = socket;
+  }
 
-function addBid(bid) {
-  const bids = document.getElementById('auctions');
-  const newBid = document.createElement('li');
-  newBid.innerHTML = bid;
-  bids.appendChild(newBid);
-}
+  makeBid(event) {
+    event.preventDefault();
+    let inputBid = document.getElementById('bid');
+    this.socket.emit('send_bid', inputBid.value);
+    inputBid.value = '';
+    return false;
+  }
 
-function setupAuctionSocketHandlers() {
-  socket.on('send_bid', addBid);
+  addBid(bid) {
+    const bids = document.getElementById('auctions');
+    const newBid = document.createElement('li');
+    newBid.innerHTML = bid;
+    bids.appendChild(newBid);
+  }
+
+  setupAuctionSocketHandlers() {
+    this.socket.on('send_bid', this.addBid);
+  }
 }
+let auction = undefined;
 
 function loadAuctionApp() {
+  auction = new Auction(socket);
   const bid = document.getElementById('bid-form');
-  bid.onsubmit = makeBid;
-
-  setupAuctionSocketHandlers();
+  bid.onsubmit = (event) => {
+    return auction.makeBid(event);
+  };
+  auction.setupAuctionSocketHandlers();
 }
